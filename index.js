ff7a9ad649c6f44fe473dbaf6176a4aba9c0dc08
unknown <ready.kresna@ui.ac.id>
2024-02-28 20:16:11 +0700

    add log new

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-var a = 1
-var b = 2
 
-if (a < b) console.log("b more than a");
 
-console.log("ini feature online payment");
-console.log("ini feature online payment");
+//orang mau lamar kerja syaratnya dia harus berumur 17 tahun keatas
+//dia harus berasal dari kota gotham
+//misalnya dia dibawah 30 tahun dia masuk ke divisi junior
+//diatas 30 tahun keatas masuk ke divisi senior
+//memenuhi print "selamat keterima di divisi junior" atau sebaliknya 
+
+var umur = 20
+var asal = "gotham"
+
+if (umur < 30 && asal === "gotham") console.log("Selamat keterima di divisi junior")
+if (umur >= 30 && asal === "gotham") console.log("Selamat keterima di divisi senior")
+else console.log ("Maaf anda tidak memenuhi kriteria")  
+
