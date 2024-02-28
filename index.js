f3cf34c49073a74279ce94a8081bed6858e01a88
William <mauritius.william.wijaya1@gmail.com>
2024-02-28 20:53:21 +0700

    pembelajaran loop, declare variable, scoope, git workflow

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-console.log("ini feature online payment");
-console.log("ini feature online payment");
+//? SCOOP
+let a = 1 // asign
+let b = 2
+
+// cara comment : ctrl + /
+
+
+a = a + 1 // re asign
+a = a + 1 // re asign
+a < 3 // condition
+
+// if (a < b) {
+//     console.log("Hello world!");
+// }
+
+//? FOR LOOP
+for (let i = 0; i < 10; i = i + 2) {
+    console.log("hello");
+}
